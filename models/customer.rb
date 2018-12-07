8d3d7af71b89eb9c6b7c97375841eea2ad59636a
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-07 14:42:13 +0000

    Initialise Customer class.

--- models/customer.rb
+++ models/customer.rb
@@ -1,1 +1,14 @@
 
+require_relative('../db/sq_runner')
+
+
+class Customer
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to__i
+  end
+
+
+end
