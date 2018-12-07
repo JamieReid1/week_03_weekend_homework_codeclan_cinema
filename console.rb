54dae85d2ce346ad52963ecf67e854cb0c5a8695
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-07 15:41:38 +0000

    Create ticket and save ticket method.

--- console.rb
+++ console.rb
@@ -11,3 +11,6 @@ customer1.save()
 
 film1 = Film.new({ 'title' => 'The Magnificent Seven', 'price' => 10 })
 film1.save()
+
+ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id })
+ticket1.save()
---
+++ models/ticket.rb
@@ -0,0 +1,24 @@
+
+require_relative('../db/sql_runner')
+
+
+class Ticket
+
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id']
+    @film_id = options['film_id'].to_i
+  end
+
+
+  def save()
+    sql = "INSERT INTO tickets ( customer_id, film_id ) VALUES ( $1, $2 ) RETURNING *"
+    values = [@customer_id, @film_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+
+end
