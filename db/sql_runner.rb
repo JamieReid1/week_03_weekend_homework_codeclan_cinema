54ae649705b262c620ebedca8600347d00e43f21
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-07 14:39:48 +0000

    Create sql runner.

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,1 +1,20 @@
 
+require('pg')
+
+
+class SqlRunner
+
+
+  def self.run(sql, values = [ ])
+    begin
+      db = PG.connect({ dbname: 'bookings', host: 'localhost' })
+      db.prepare('run', sql)
+      result = db.exec_prepared('run', values)
+    ensure
+      db.close() if db != nil
+    end
+    return SqlRunner.run(sql, values)
+  end
+
+
+end
